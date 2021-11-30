0c7616138aa5e8d328db027bd946ec1ed1212eed
Finley Edwards <EDW17006550@btc.ac.uk>
2021-11-30 16:23:17 +0000

    3

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -31,6 +31,8 @@ namespace ConsoleApp6
             }
 
             Console.WriteLine(LSearch(nums, 12));
+
+            Console.WriteLine(BSearch(nums, 12, 0, 11));
         }
 
         private static int LSearch(List<int> list, int itemToFind)
@@ -38,7 +40,32 @@ namespace ConsoleApp6
             for (int i = 0; i < list.Count; i++)
             {
                 if (list[i] == itemToFind)
+                {
                     return i;
+                }
+            }
+            return -1;
+        }
+
+        private static int BSearch(List<int> list, int itemToFind, int min, int max)
+        {
+            int midPoint = (min + max) / 2;
+            
+            if (list[midPoint] == itemToFind)
+            {
+                return midPoint;
+            }
+            else if (list[midPoint] < itemToFind)
+            {
+                BSearch(list, itemToFind, midPoint + 1, max);
+            }
+            else if (list[midPoint] > itemToFind)
+            {
+                BSearch(list, itemToFind, min, midPoint - 1);
+            }
+            else
+            {
+                return midPoint;
             }
             return -1;
         }

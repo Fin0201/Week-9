98f3b0212ed092a99f8b18815050095bb13d20f5
Finley Edwards <EDW17006550@btc.ac.uk>
2021-11-30 15:50:02 +0000

    1

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace ConsoleApp6
 {
@@ -6,7 +8,39 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> nums = new ()
+            {
+                3,
+                8,
+                1,
+                4,
+                7,
+                6,
+                9,
+                2,
+                12,
+                11,
+                10,
+                5
+            };
+            
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(LSearch(nums, 12));
+        }
+
+        private static int LSearch(List<int> list, int itemToFind)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == itemToFind)
+                    return i;
+            }
+            return -1;
         }
     }
 }
